177908193f0c994e59e400333152bdc2e528b783
Jacob Plumb <jacobmplumb@outlook.com>
2019-10-21 00:18:27 -0400

    initial commit

---
+++ InSeasonAppApi/Models/User.cs
@@ -0,0 +1,9 @@
+﻿using System;
+namespace InSeasonAppApi.Models
+{
+    public class User
+    {
+        public string UserName { get; set; }
+        public string Password { get; set; }
+    }
+}
